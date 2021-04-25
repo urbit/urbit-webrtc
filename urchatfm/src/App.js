9f978b9d2387a7f2bdbe2dcef006ca1da73a9cb1
Edward Amsden <edward@blackriversoft.com>
2021-04-24 22:49:36 -0400

    urchatfm: testing

--- urchatfm/src/App.js
+++ urchatfm/src/App.js
@@ -1,25 +1,51 @@
 import './App.css';
 import Login from './Login.js';
 import React from 'react';
-import {UrbitRTCApp} from 'switchboard';
-import {useState, useEffect, useRef} from 'react';
+import { UrbitRTCApp } from 'switchboard';
+import Icepond from 'icepond';
+import { useState, useEffect, useRef } from 'react';
 
-// TODO:
-// oops
-// test and bugfix
-// call picked up event
-// *hangup button*
+function WithIcepond(urbit, component, configuration = {}) {
+  const icepondRef = useRef(new Icepond(urbit));
+  const [iceServers, setIceServers] = useState([]);
+  useEffect(() => {
+    icepondRef.current.oniceserver = (evt) => {
+      const server = evt.newIceServer;
+      setIceServers(servers => servers.concat([server]));
+      console.log('Got server: ', server);
+    };
+    icepondRef.current.initialize();
+  }, [urbit]);
+  console.log('Servers:', iceServers);
+  const fixedServers = (typeof configuration.iceServers === 'undefined') ? [] : configuration.iceServers;
+  const iceConf = { iceServers: fixedServers.concat(iceServers) };
+  return component(urbit, { ...configuration, ...iceConf });
+}
 
 function WithUrbitRTCApp(urbit, dap, configuration, component) {
   const urbitRtcAppRef = useRef(new UrbitRTCApp(dap, configuration, urbit));
   const urbitRtcApp = urbitRtcAppRef.current;
   useEffect(() => {
-    urbitRtcApp.configuration = configuration;
-    urbitRtcApp.dap = dap;
     urbitRtcApp.urbit = urbit;
-    urbitRtcApp.onerror = (err) => console.log(`Urbit RTC app error: ${err.toString()}`);
-  }, [urbit, dap, configuration]);
-  
+  // eslint-disable-next-line
+  }, [urbit]);
+
+  useEffect(() => {
+    urbitRtcApp.dap = dap;
+  // eslint-disable-next-line
+  }, [dap]);
+
+  useEffect(() => {
+    urbitRtcApp.configuration = configuration;
+    console.log('urbitRtcApp.configuration set to', configuration);
+  // eslint-disable-next-line
+  });
+
+  useEffect(() => {
+    urbitRtcApp.onerror = err => console.log(`Urbit RTC app error: ${err.toString()}`);
+  // eslint-disable-next-line
+  }, []);
+
   return component(urbit, urbitRtcAppRef.current);
 }
 
@@ -33,36 +59,40 @@ function Urchat(urbit, urbitRtcApp) {
 
   // Set up the incoming call handler
   useEffect( () => {
-    urbitRtcApp.addEventListener("incomingcall", (incomingCallEvt) => {
+    urbitRtcApp.addEventListener('incomingcall', (incomingCallEvt) => {
       if(incomingCall === null && ongoingCall === null) {
         setIncomingCall(incomingCallEvt);
-        console.log("incoming: ", incomingCallEvt);
+        console.log('incoming: ', incomingCallEvt);
       } else {
         incomingCallEvt.reject();
-        console.log("rejected: ", incomingCallEvt);
+        console.log('rejected: ', incomingCallEvt);
       }
     });
+  // eslint-disable-next-line
   }, [urbitRtcApp]);
 
   return (
     <React.Fragment>
-    {(incomingCall === null) ? "" : (<UrchatIncoming incoming={incomingCall} setIncoming={setIncomingCall} setOngoing={setOngoingCall} setIsCaller={setIsCaller} />)}
-    {(ongoingCall === null) ? "" : (<UrchatChat ongoing={ongoingCall} setOngoing={setOngoingCall} isCaller={isCaller} />)}
-    {(incomingCall === null && ongoingCall === null) ?  (<UrchatCall setOngoing={setOngoingCall} setIsCaller={setIsCaller} urbitRtcApp={urbitRtcApp} />) : ""}
+    <IceServerList servers={ urbitRtcApp.configuration.iceServers } />
+    {(incomingCall === null) ? '' : (<UrchatIncoming incoming={incomingCall} setIncoming={setIncomingCall} setOngoing={setOngoingCall} setIsCaller={setIsCaller} />)}
+    {(ongoingCall === null) ? '' : (<UrchatChat ongoing={ongoingCall} setOngoing={setOngoingCall} isCaller={isCaller} />)}
+    {(incomingCall === null && ongoingCall === null) ?  (<UrchatCall setOngoing={setOngoingCall} setIsCaller={setIsCaller} urbitRtcApp={urbitRtcApp} />) : ''}
     </React.Fragment>
   );
 }
 
-function UrchatIncoming({incoming, setIncoming, setOngoing, setIsCaller}) {
+function UrchatIncoming({ incoming, setIncoming, setOngoing, setIsCaller }) {
   const answer = () => {
-    setOngoing({call: incoming.call, conn: incoming.answer()});
+    const conn = incoming.answer();
+    conn.initialize();
+    setOngoing({ call: incoming.call, conn: conn });
     setIncoming(null);
     setIsCaller(false);
-  }
+  };
 
   const reject = () => {
     incoming.reject().then(() => setIncoming(null));
-  }
+  };
 
   return (
     <div className="incoming-call">
@@ -70,66 +100,86 @@ function UrchatIncoming({incoming, setIncoming, setOngoing, setIsCaller}) {
       <button type="button" onClick={answer}>Answer</button>
       <button type="button" onClick={reject}>Reject</button>
     </div>
-  )
+  );
 }
 
-function UrchatChat({ongoing, setOngoing, isCaller}) {
+// eslint-disable-next-line
+function UrchatChat({ ongoing, setOngoing, isCaller }) {
   const [dataChannel, setDataChannel] = useState(null);
   const [dataChannelOpen, setDataChannelOpen] = useState(false);
   const [messages, setMessages] = useState([]);
-  const [composedMessage, setComposedMessage] = useState("");
+  const [composedMessage, setComposedMessage] = useState('');
 
   const incomingChatMessage = (evt) => {
-    setMessages(messages => messages.concat([{sender: ongoing.call.peer, message: evt.data}]));
-  }
+    setMessages(messages => messages.concat([{ sender: ongoing.call.peer, message: evt.data }]));
+  };
 
   const sendMessage = (evt) => {
-    evt.preventDefault()
+    evt.preventDefault();
     dataChannel.send(composedMessage);
-    setMessages(messages.concat([{sender: "me", message: composedMessage}]));
-    setComposedMessage("");
-  }
+    setMessages(messages.concat([{ sender: 'me', message: composedMessage }]));
+    setComposedMessage('');
+  };
 
   const hangup = () => {
     ongoing.conn.close();
     setOngoing(null);
-  }
+  };
 
   const hungup = () => {
     setOngoing(null);
-  }
+  };
 
   const setupDataChannel = () => {
-    const newDataChannel = ongoing.conn.createDataChannel("urchatfm", {});
+    const newDataChannel = ongoing.conn.createDataChannel('urchatfm', {});
     setDataChannel(newDataChannel);
-    newDataChannel.addEventListener("open", () => { console.log("Data channel open"); setDataChannelOpen(true)});
-    newDataChannel.addEventListener("close", () => { console.log("Data channel closed"); setDataChannelOpen(false)});
-    newDataChannel.addEventListener("message", incomingChatMessage);
+    newDataChannel.addEventListener('open', () => {
+      console.log('Data channel open');
+      setDataChannelOpen(true);
+    });
+    newDataChannel.addEventListener('close', () => {
+      console.log('Data channel closed');
+      setDataChannelOpen(false);
+    });
+    newDataChannel.addEventListener('message', incomingChatMessage);
   };
 
   const receiveDataChannel = (evt) => {
     const newDataChannel = evt.channel;
     setDataChannel(newDataChannel);
-    newDataChannel.addEventListener("open", () => { console.log("Data channel open"); setDataChannelOpen(true)});
-    newDataChannel.addEventListener("close", () => { console.log("Data channel closed"); setDataChannelOpen(false)});
-    newDataChannel.addEventListener("message", incomingChatMessage);
-  }
+    newDataChannel.addEventListener('open', () => {
+      console.log('Data channel open');
+      setDataChannelOpen(true);
+    });
+    newDataChannel.addEventListener('close', () => {
+      console.log('Data channel closed');
+      setDataChannelOpen(false);
+    });
+    newDataChannel.addEventListener('message', incomingChatMessage);
+  };
 
   useEffect(() => {
     if(isCaller) {
-      ongoing.conn.addEventListener("statechanged", (evt) => { console.log("state changed", evt.urbitState); if(evt.urbitState === "connected") { setupDataChannel() }});
+      ongoing.conn.addEventListener('statechanged', (evt) => {
+        console.log('state changed', evt.urbitState);
+        if(evt.urbitState === 'connected') {
+          setupDataChannel();
+        }
+      });
     } else {
-      ongoing.conn.addEventListener("datachannel", receiveDataChannel)
+      ongoing.conn.addEventListener('datachannel', receiveDataChannel);
     }
+    ongoing.conn.addEventListener('hungupcall', hungup);
+  // eslint-disable-next-line
   }, [ongoing]);
 
-  const UrchatMessage = ({sender, message}) => {
+  const UrchatMessage = ({ sender, message }) => {
     return (
       <div className="msg">
-        <span className="msg-sender" style={{fontWeight: "bold"}}>{sender}: </span>
+        <span className="msg-sender" style={{ fontWeight: 'bold' }}>{sender}: </span>
         <span className="msg-content">{message}</span>
       </div>
-    )
+    );
   };
 
   return (
@@ -140,21 +190,48 @@ function UrchatChat({ongoing, setOngoing, isCaller}) {
           ))}
       </div>
       <form onSubmit={sendMessage}>
-        <input type="text" value={composedMessage} onChange={(evt) => setComposedMessage(evt.target.value)}/>
-        <input type="submit" value="Send" disabled={dataChannel === null || ! dataChannelOpen}/>
+        <input type="text" value={composedMessage} onChange={evt => setComposedMessage(evt.target.value)} />
+        <input type="submit" value="Send" disabled={dataChannel === null || ! dataChannelOpen} />
       </form>
       <button type="button" onClick={hangup}>Hang Up</button>
     </div>
-  )
+  );
 }
 
-function UrchatCall({setOngoing, setIsCaller, urbitRtcApp}) {
-  const [ship, setShip] = useState("zod");
-  
+function IceServerList({ servers }) {
+  console.log('Servers sent to component: ', servers);
+  const serverUrls = [];
+  for(const i in servers) {
+    const server = servers[i];
+    console.log(server);
+    console.log('Type of server.urls: ', typeof server.urls);
+    if(typeof server.urls === 'string') {
+        serverUrls.push(server.urls);
+    } else {
+      for(const j in server.urls) {
+        serverUrls.push(server.urls[j]);
+      }
+    }
+  }
+  return (
+    <div>
+      <h2>ICE servers</h2>
+      <ul>{ serverUrls.map((url, i) => (
+        <li key={ i }><code style={{ display: 'inline' }} >{ url } </code></li>
+      ))}
+      </ul>
+    </div>
+   );
+}
+
+function UrchatCall({ setOngoing, setIsCaller, urbitRtcApp }) {
+  const [ship, setShip] = useState('zod');
+
   const placeCall = (evt) => {
     evt.preventDefault();
-    const conn = urbitRtcApp.call(ship, "urchatfm");
-    const call = { peer: ship, dap: "urchatfm", uuid: conn.uuid };
+    const conn = urbitRtcApp.call(ship, 'urchatfm');
+    conn.initialize();
+    const call = { peer: ship, dap: 'urchatfm', uuid: conn.uuid };
     setOngoing({ conn: conn, call: call });
     setIsCaller(true);
   };
@@ -163,17 +240,18 @@ function UrchatCall({setOngoing, setIsCaller, urbitRtcApp}) {
     <form onSubmit={placeCall}>
       <label>
         Ship:
-        <input type="text" value={ship} onChange={(evt) => setShip(evt.target.value)} />
+        <input type="text" value={ship} onChange={evt => setShip(evt.target.value)} />
       </label>
-      <input type="submit" value="Call"/>
+      <input type="submit" value="Call" />
     </form>
   );
 }
 
 function App() {
-  const rtcConfig = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]};
-  return Login((urbit) => {
-    return WithUrbitRTCApp(urbit, 'urchatfm', rtcConfig, Urchat)
+  return Login(({ urbit }) => {
+    return WithIcepond( urbit, (urbit, configuration) => {
+      return WithUrbitRTCApp(urbit, 'urchatfm', configuration, Urchat);
+    });
   });
 }
 
--- urchatfm/src/Login.js
+++ urchatfm/src/Login.js
@@ -1,18 +1,26 @@
-import Urbit from '@urbit/http-api'
-import useStatefulRef from '@bedrock-layout/use-stateful-ref'
-import { useState } from 'react'
+import Urbit from '@urbit/http-api';
+import useStatefulRef from '@bedrock-layout/use-stateful-ref';
+import { useState } from 'react';
+import React from 'react';
 
-function Login(after) {
-  const urbit = useStatefulRef(null)
-  const [urbitErr, setUrbitErr] = useState("");
+// eslint-disable-next-line
+function Login(After) {
+  const urbit = useStatefulRef(null);
+  const [urbitErr, setUrbitErr] = useState('');
   const [awaitingUrbit, setAwaitingUrbit] = useState(false);
-  const [ship, setShip] = useState("zod");
-  const [url, setUrl] = useState("localhost:8081");
-  const [code, setCode] = useState("lidlut-tabwed-pillex-ridrup");
+  const [ship, setShip] = useState('zod');
+  const [url, setUrl] = useState('localhost:8081');
+  const [code, setCode] = useState('lidlut-tabwed-pillex-ridrup');
   const handleSubmit = (event) => {
-    Urbit.authenticate({ship: ship, url: url, code: code, 'verbose': true})
-       .then((ur) => { urbit.current = ur; setAwaitingUrbit(false); })
-       .catch((err) => { setUrbitErr(err.toString()); setAwaitingUrbit(false); })
+    Urbit.authenticate({ ship: ship, url: url, code: code, 'verbose': true })
+      .then((ur) => {
+        urbit.current = ur;
+        setAwaitingUrbit(false);
+      })
+      .catch((err) => {
+        setUrbitErr(err.toString());
+        setAwaitingUrbit(false);
+      });
     setAwaitingUrbit(true);
     event.preventDefault();
   };
@@ -21,26 +29,28 @@ function Login(after) {
       <form onSubmit={handleSubmit}>
         <label>
           Ship:
-          <input type="text" value={ship} onChange={(event) => setShip(event.target.value)} disabled={awaitingUrbit}/>
+          <input type="text" value={ship} onChange={event => setShip(event.target.value)} disabled={awaitingUrbit} />
         </label>
         <label>
           URL:
-          <input type="text" value={url} onChange={(event) => setUrl(event.target.value)} disabled={awaitingUrbit}/>
+          <input type="text" value={url} onChange={event => setUrl(event.target.value)} disabled={awaitingUrbit} />
         </label>
         <label>
           Code:
-          <input type="password" value={code} onChange={(event) => setCode(event.target.value)} disabled={awaitingUrbit}/>
+          <input type="password" value={code} onChange={event => setCode(event.target.value)} disabled={awaitingUrbit} />
         </label>
-        <input type="submit" value="Connect" disabled={awaitingUrbit}/>
+        <input type="submit" value="Connect" disabled={awaitingUrbit} />
       </form>
       <span className="Urbit-conn">
-        {urbit.current == null ? "disconnected" : urbit.current.channelUrl}
+        {urbit.current === null ? 'disconnected' : urbit.current.channelUrl}
       </span>
       <div className="Urbit-err">
         {urbitErr}
       </div>
       <div className="Urbit-embedded-app">
-        {after(urbit.current)}      
+        {urbit.current === null ? '' : (
+         <After urbit={urbit.current} />
+        )}
       </div>
     </div>
   );
